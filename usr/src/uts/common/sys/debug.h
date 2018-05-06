dd2801927af4eddf8beaa3ab9149b0646256edc9
Joshua M. Clulow <josh@sysmgr.org>
2018-05-05 22:55:44 -0700

    VERIFY macros must execute the expression

--- usr/src/uts/common/sys/debug.h
+++ usr/src/uts/common/sys/debug.h
@@ -1,9 +1,14 @@
 #ifndef	_SYS_DEBUG_H
 #define	_SYS_DEBUG_H
 
+#include <stdlib.h>
+
 #define	ASSERT(cond)
 
-#define	VERIFY0(cond)
-#define	VERIFY3U(a, op, b)
+#define	VERIFY(EX)	do { if (!(EX)) { abort(); } } while (0)
+
+#define	VERIFY0(cond)	VERIFY((cond) == 0)
+#define	VERIFY3U(a, op, b)	VERIFY((a) op (b))
+#define	VERIFY3S(a, op, b)	VERIFY((a) op (b))
 
 #endif /* _SYS_DEBUG_H */
